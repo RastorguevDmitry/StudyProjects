35b5f3fc53d155bad04e663ea94bb4026abafd93
Dmitry <rdi81@yandex.ru>
2019-07-03 22:47:47 +0300

    ChangeFirstLastWordsInLine

--- src/main/java/JavaIO/HomeTask/ChangeFirstLastWordsInLine.java
+++ src/main/java/JavaIO/HomeTask/ChangeFirstLastWordsInLine.java
@@ -3,13 +3,13 @@ package JavaIO.HomeTask;
 import java.io.*;
 import java.util.ArrayList;
 
-
 //Прочитать строки из файла и поменять местами первое и последнее слова в каждой строке.
 
 public class ChangeFirstLastWordsInLine {
 
     public static void main(String[] args) {
 
+        ArrayList<String> linesAfterChange = new ArrayList<>();
 
         try (FileReader fileReader = new FileReader("src" + File.separator + "main"
                 + File.separator + "resources" + File.separator + "ChangeFirstLastWordsInLine.txt");
@@ -17,41 +17,45 @@ public class ChangeFirstLastWordsInLine {
             ArrayList<String> linesFromFile = new ArrayList<>();
             bufferedReader.lines().forEach(linesFromFile::add);
 
-            ArrayList<String> linesAfterChange = new ArrayList<>();
-            linesAfterChange.addAll(ChangeFirstLastWordsInLine(linesFromFile)) ;
+            linesAfterChange.addAll(ChangeFirstLastWordsInLine(linesFromFile));
 
-            System.out.println(linesAfterChange);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        try (FileWriter fileWriter = new FileWriter("src" + File.separator + "main"
+                + File.separator + "resources" + File.separator + "ChangeFirstLastWordsInLine.txt");
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
 
+            for (String lineAfterChange :
+                    linesAfterChange) {
+                bufferedWriter.write(lineAfterChange);
+                bufferedWriter.newLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     static ArrayList<String> ChangeFirstLastWordsInLine(ArrayList<String> linesForChange) {
         ArrayList<String> linesWithChangedFirstLastWords = new ArrayList<>();
-
+        String allWordsInLineAfterChange;
         for (String allWordsInLineBeforChange :
                 linesForChange) {
-            String allWordsInLineAfterChange = null;
-
+            allWordsInLineAfterChange =
+                    allWordsInLineBeforChange.split(" ")[allWordsInLineBeforChange.split(" ").length - 1] + " ";
             if (allWordsInLineBeforChange.split(" ").length > 1) {
-                String firstWordInLine = allWordsInLineBeforChange.split(" ")[0];
-                String lastWordInLine = allWordsInLineBeforChange.split(" ")[allWordsInLineBeforChange.split(" ").length - 1];
-                allWordsInLineAfterChange = lastWordInLine + " ";
                 for (int i = 1; i < allWordsInLineBeforChange.split(" ").length - 1; i++) {
                     allWordsInLineAfterChange += allWordsInLineBeforChange.split(" ")[i] + " ";
                 }
-                allWordsInLineAfterChange += firstWordInLine;
+                allWordsInLineAfterChange += allWordsInLineBeforChange.split(" ")[0];
             }
             linesWithChangedFirstLastWords.add(allWordsInLineAfterChange);
         }
-
-
         return linesWithChangedFirstLastWords;
     }
-
 
 }
